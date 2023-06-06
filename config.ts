d12e50ce7dde4488352bc65dd061e0a5adb6c51b
mookmac <mookmac@users.noreply.github.com>
2023-06-06 21:25:17 +0000

    first commit

---
+++ config.ts
@@ -0,0 +1,7 @@
+const config = {
+  botId: process.env.BOT_ID,
+  botPassword: process.env.BOT_PASSWORD,
+  OpenAIKey: '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',
+};
+
+export default config;

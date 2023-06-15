90de51bb2893ea75e41041d6337134eb07bcb1f3
mookmac <mookmac@gmail.com>
2023-06-15 16:56:31 +0100

    updates as at 1656 BST

---
+++ actions/meetingNotesAction.ts
@@ -0,0 +1,50 @@
+import { TurnContext } from 'botbuilder';
+import { Application, ConversationHistory } from '@microsoft/teams-ai';
+import { ApplicationTurnState } from '../index';
+import { IEmployee, IObjective, IDataEntities } from '../interfaces';
+import { stat } from 'fs';
+
+/**
+ * @param app
+ */
+export function meetingNotesAction(app: Application<ApplicationTurnState>): void {
+    app.ai.action('meetingNotes', async (context: TurnContext, state: ApplicationTurnState, data: IDataEntities) => {
+        const action = (data.operation ?? '').toLowerCase();
+        switch (action) {
+            case 'save':
+                return await saveMeetingNotes(app, context, state);
+            default:
+                await context.sendActivity(`[map.${action}]`);
+                return true;
+        }
+      });
+}
+
+/**
+ * @param app
+ * @param context
+ * @param state
+ */
+async function saveMeetingNotes(
+    app: Application<ApplicationTurnState>,
+    context: TurnContext,
+    state: ApplicationTurnState
+): Promise<boolean> {
+    // Use the employee object to answer the human
+    const newResponse = await app.ai.completePrompt(context, state, 'suggestTalkingPoints');
+    if (newResponse) {
+        if (state.conversation.value.talkingPointSuggestions == undefined)
+        {
+            state.conversation.value.talkingPointSuggestions = [newResponse];
+        }
+        else
+        {
+            state.conversation.value.talkingPointSuggestions.push(newResponse);
+        }
+        await context.sendActivity(newResponse);
+    } else {
+        await context.sendActivity("Error finding that information. Please try again.");
+    }
+
+    return false;
+}
